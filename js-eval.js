dccff7180636531c058592f739245d90412730e6
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-10-03 15:00:08 +0300

    pus coment-uri pe codul care nu cred că poate ajuta

--- js-eval.js
+++ js-eval.js
@@ -1,43 +1,67 @@
-//check: https://24ways.org/2005/dont-be-eval/
+// //check: https://24ways.org/2005/dont-be-eval/
 
-console.log('abc')
+// console.log('abc')
 
-function looseJsonParse(obj) {
-    return eval(`(${obj})`);
-}
+// function looseJsonParse(obj) {
+//     return eval(`(${obj})`);
+// }
 
-console.log(looseJsonParse(
-    "{a:(4-1), b:function(){}, c:new Date()}"
-))
-
-
-//
-
-let x = 10;
-let y = 20;
-let text = "x * y";
-let result = eval(text);
-console.log(result)
+// console.log(looseJsonParse(
+//     "{a:(4-1), b:function(){}, c:new Date()}"
+// ))
 
 
-let text2 = "2 * 3";
-let result2 = eval(text2);
-console.log(result2)
+// //
+
+// let x = 10;
+// let y = 20;
+// let i = "x * y";
+// let e = eval(text);
+// console.log(result)
 
 
-let text3 = `
-function myFun(a, b){
-    return a + b
-}
+// let text2 = "2 * 3";
+// let result2 = eval(text2);
+// console.log(result2)
 
-myFun(2,3)
-`
 
-let result3 = eval(text3);
-console.log(result3)
+// let text3 = `
+// function myFun(a, b){
+//     return a + b
+// }
+
+// myFun(2,3)
+// `
+
+// var result3 = eval(text3);
+// console.log(result3)
 
 
 var lim = 3;
 var body = 'return lim+1;';
-var newVal = eval('(lim) => {' + body + '}')(7); //returns "4"
+var newVal = eval('(lim) => {' + body + '}')(lim); //returns "4"
 console.log(newVal);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
