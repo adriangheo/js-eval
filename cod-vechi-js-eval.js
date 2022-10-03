5b6fde58d42222255a91e276cd2c6259bcdf0349
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-10-03 15:07:06 +0300

    lăsat doar codul potential util

---
+++ cod-vechi-js-eval.js
@@ -0,0 +1,38 @@
+//check: https://24ways.org/2005/dont-be-eval/
+
+console.log('abc')
+
+function looseJsonParse(obj) {
+    return eval(`(${obj})`);
+}
+
+console.log(looseJsonParse(
+    "{a:(4-1), b:function(){}, c:new Date()}"
+))
+
+
+//
+
+let x = 10;
+let y = 20;
+let i = "x * y";
+let e = eval(text);
+console.log(result)
+
+
+let text2 = "2 * 3";
+let result2 = eval(text2);
+console.log(result2)
+
+
+let text3 = `
+function myFun(a, b){
+    return a + b
+}
+
+myFun(2,3)
+`
+
+var result3 = eval(text3);
+console.log(result3)
+
--- js-eval.js
+++ js-eval.js
@@ -1,47 +1,8 @@
-// //check: https://24ways.org/2005/dont-be-eval/
-
-// console.log('abc')
-
-// function looseJsonParse(obj) {
-//     return eval(`(${obj})`);
-// }
-
-// console.log(looseJsonParse(
-//     "{a:(4-1), b:function(){}, c:new Date()}"
-// ))
-
-
-// //
-
-// let x = 10;
-// let y = 20;
-// let i = "x * y";
-// let e = eval(text);
-// console.log(result)
-
-
-// let text2 = "2 * 3";
-// let result2 = eval(text2);
-// console.log(result2)
-
-
-// let text3 = `
-// function myFun(a, b){
-//     return a + b
-// }
-
-// myFun(2,3)
-// `
-
-// var result3 = eval(text3);
-// console.log(result3)
-
-
 var atribut1 = 3;
 var atribut2 = 10;
 var atribut3 = 1000;
-var body = 'return atribut1 + atribut2 + atribut3 + 1;';
-var newVal = eval('(atribut1,atribut2, atribut3) => {' + body + '}')(atribut1, atribut2, atribut3); //returns "4"
+var codDeLaNoi = 'return atribut1 + atribut2 + atribut3 + 1;';
+var newVal = eval('(atribut1,atribut2, atribut3) => {' + codDeLaNoi + '}')(atribut1, atribut2, atribut3); //returns "4"
 console.log(newVal);
 
 
