3657a6cb6baf1b95c434b2ecee963d261b567cd7
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-10-03 06:32:29 +0300

    hmmm

--- alternative-to-eval.js
+++ alternative-to-eval.js
@@ -7,3 +7,12 @@ function looseJsonParse(obj) {
 console.log(looseJsonParse(
     "{a:(4-1), b:function(){}, c:new Date()}"
 ))
+
+//
+
+
+let x = 10;
+let y = 20;
+let text = "x * y";
+let result = eval(text);
+console.log(Function(`return (${result})`))
--- js-eval.js
+++ js-eval.js
@@ -1,3 +1,5 @@
+//check: https://24ways.org/2005/dont-be-eval/
+
 console.log('abc')
 
 function looseJsonParse(obj) {
@@ -7,3 +9,12 @@ function looseJsonParse(obj) {
 console.log(looseJsonParse(
     "{a:(4-1), b:function(){}, c:new Date()}"
 ))
+
+
+//
+
+let x = 10;
+let y = 20;
+let text = "x * y";
+let result = eval(text);
+console.log(result)
