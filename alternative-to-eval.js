a591767042b1bc6ab43e45b867405040e9f35d68
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-10-03 06:20:24 +0300

    alternative to eval

---
+++ alternative-to-eval.js
@@ -0,0 +1,9 @@
+console.log('cba')
+
+function looseJsonParse(obj) {
+    return Function(`"use strict";return (${obj})`)();
+}
+
+console.log(looseJsonParse(
+    "{a:(4-1), b:function(){}, c:new Date()}"
+))
